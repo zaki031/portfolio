36787518388bfe9c577585751cfb52a1574b7c76
zaki031 <115791170+zaki031@users.noreply.github.com>
2023-05-28 13:40:57 +0200

    edit

--- pages/about.js
+++ pages/about.js
@@ -20,10 +20,9 @@ const about = () => {
         <p className="text-2xl ">I'm zaki, a frontend developer based in Oran,Algeria and I really love coding! <br />
             beside the technologies I use in the web develoment, I also use other languages like Python,Golang,C#, I might be a webdeveloper but u can find me messing with cybersecurity stuff xD
             you can check my <a href="" className="font-bold underline">github</a>  if you are interested in my other projects. <br />
-            Next summer, I'm planning to dive into the cybersecurity world I will try to get better with Python and Typescript and maybe learn React-Native. <br /><br />
-            As you know, I'm a high school student and I'm on my final year. I planning to get into a computer science university or anything related to technologies. <br /><br />
-            In my free time, I like to code haha or playing chess, reading tech articles/books and watching movies.
-            OH yeah, I was born in the 21th sept 2005 if you want to gift me something if you know what i mean :D <br /><br />
+            Next summer, I'm planning to dive into the cybersecurity world and I will try to get better with Python and Typescript. <br /><br />
+            As you know, I'm a high school student and I'm on my final year. I planning to get into a computer science university or anything related with technology. <br /><br />
+            In my free time, I like to code haha or playing chess, reading tech articles/books and watching movies.<br /><br />
             Feel free to contact me for collabs or asking me more question on my social media accounts
 
         </p>
